ffd76d43a0353c17c257f2f79dc85ac6b075ac4f
Rhaelston Honório <rhaelston@hotmail.com>
2024-10-13 18:03:17 -0300

    absulote

--- src/pages/TimerFlor.tsx
+++ src/pages/TimerFlor.tsx
@@ -39,7 +39,7 @@ export const TimerFlor: React.FC = () => {
             <div className='px-2'>{seconds}  Segundos </div>
             </div>
           </div>
-          <footer className="bg-blue-600 text-white p-4 mt-8">
+          <footer className="bg-blue-600 text-white p-4 bottom-0 absolute w-screen">
           <div className="container mx-auto text-center">
             <p>
               <a href="https://www.instagram.com/ju_martins_23/" className="hover:underline">Olha a Beleza dessa mulher!</a> 
